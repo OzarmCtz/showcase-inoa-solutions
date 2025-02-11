3c63b75a8a8ad5a591d1b785ad93b101c6cecd47
OzarmCtz <167234820+OzarmCtz@users.noreply.github.com>
2025-02-11 14:45:02 -0800

    work on text about us

--- app/components/about/InoaWhyChooseUs.vue
+++ app/components/about/InoaWhyChooseUs.vue
@@ -21,19 +21,35 @@ const links = [
     description="Watch your dreams materialize before your eyes with us."
   />
   <ULandingSection
-    headline="Pro"
-    title="The freedom to build anything"
+    title="1. Plus de 10 ans d'expériences"
     description="Nuxt UI Pro ships with an extensive set of advanced components that cover a wide range of use-cases."
     align="left"
+    :ui="{
+      strategy: 'override',
+      description: 'text-lg mt-10',
+      title: 'text-4xl font-bold tracking-tight text-primary',
+    }"
   >
+    <template #description>
+      Vidéoprotection, alarme intrusion, contrôle d’accès, caméra nomade…
+      <br />
+      <br />
+      Notre slogan, « Architecte de votre protection », reflète parfaitement
+      notre volonté d’innovation et d’accompagnement au service de votre
+      protection.
+      <br />
+      <br />
+      Nous vous conseillons de A à Z sur l’ensemble de votre besoin de sécurité
+      électronique, du conseil jusqu’à la mise en oeuvre et l’exécution du
+      chantier.
+    </template>
     <img
-      src="https://picsum.photos/640/360"
+      src="/inoa_place.jpeg"
       class="w-full rounded-md shadow-xl ring-1 ring-gray-300 dark:ring-gray-700"
     />
   </ULandingSection>
   <ULandingSection
-    title="The freedom to build anything"
-    description="Nuxt UI Pro ships with an extensive set of advanced components that cover a wide range of use-cases."
+    title="2. Nos certifications"
     align="right"
     :links="[
       {
@@ -43,11 +59,24 @@ const links = [
         size: 'lg',
       },
     ]"
+    :ui="{
+      strategy: 'override',
+      description: 'text-lg mt-10',
+      title: 'text-4xl font-bold tracking-tight text-primary',
+    }"
   >
     <img
       src="https://picsum.photos/640/360"
       class="w-full rounded-md shadow-xl ring-1 ring-gray-300 dark:ring-gray-700"
     />
+
+    <template #description>
+      Nous détenons depuis plus de 10 ans les certifications suivantes :
+      <br /><br />Service d’Installation et de Maintenance de Systèmes de
+      Détection d’Intrusion (NF 367-I81) Catégories AB : Certificat
+      n°191/13/367-81 <br /><br />Vidéosurveillance (NF 367-I82) : Certificat n°
+      121/13/367-82
+    </template>
   </ULandingSection>
   <ULandingSection
     title="The freedom to build anything"
