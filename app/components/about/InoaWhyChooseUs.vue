258dbcb2ac63dd4775442f9a012e8da7c5482ce1
OzarmCtz <167234820+OzarmCtz@users.noreply.github.com>
2025-02-12 12:29:36 -0800

    work on about us

--- app/components/about/InoaWhyChooseUs.vue
+++ app/components/about/InoaWhyChooseUs.vue
@@ -1,20 +1,27 @@
 <script setup lang="ts">
 const links = [
   {
-    name: "Fully customizable",
+    name: "Des solutions personnalisées",
     description:
-      "Like Nuxt UI, change the style of any component from your App Config or customize them specifically through the ui prop.",
-    icon: "i-heroicons-wrench-screwdriver",
+      "Nos clients institutionnels, professionnels & particuliers nous font confiance pour la mise en oeuvre et la maintenance de leurs systèmes de vidéoprotection.",
+    icon: "icon-park-twotone:success",
   },
   {
-    name: "Slots for everything",
+    name: "Une expertise reconnue",
     description:
-      "Each component leverages the power of Vue's slots to give you the flexibility to build anything.",
-    icon: "i-heroicons-square-3-stack-3d",
+      "Grâce à notre engagement en matière de qualité et de réactivité, nous avons su nous imposer comme un acteur de confiance dans le domaine de la sécurité électronique.",
+    icon: "mdi:medal",
+  },
+  {
+    name: "Une réputation bien établie",
+    description:
+      "Cela nous permet aujourd’hui de bénéficier d’une belle réputation : vous avez sûrement déjà vu ou êtes passé devant l’un de nos panneaux « Vidéoprotégé par INOA Solutions » !",
+    icon: "icon-park-twotone:people-bottom",
   },
 ];
 
 import CertificationsModal from "@/components/CertificationsModal.vue";
+import type { list } from "postcss";
 
 const isOpen = ref(false);
 
@@ -98,22 +105,60 @@ const openModal = () => {
     </template>
   </ULandingSection>
   <ULandingSection
-    title="The freedom to build anything"
-    description="Nuxt UI Pro ships with an extensive set of advanced components that cover a wide range of use-cases."
+    title="3. La confiance de nos clients"
+    description="Nous mettons notre expertise au service de la sécurité des entreprises, institutions et particuliers, en proposant des solutions adaptées aux besoins spécifiques de chacun."
     :features="links"
     align="left"
+    :ui="{
+      strategy: 'override',
+      description: 'text-lg',
+      title: 'text-4xl font-bold tracking-tight text-primary mb-10',
+      features: {
+        wrapper: {
+          list: 'space-y-12',
+        },
+      },
+    }"
   >
-    <img
-      src="https://picsum.photos/360/640"
+    <NuxtImg
+      src="/clients-inoa-solutions.jpg"
       class="w-full rounded-md shadow-xl ring-1 ring-gray-300 dark:ring-gray-700"
+      draggable="false"
     />
   </ULandingSection>
   <ULandingSection
-    headline="Pro"
-    title="The freedom to build anything"
+    title="4. À la pointe de la technologie"
     description="Nuxt UI Pro ships with an extensive set of advanced components that cover a wide range of use-cases."
     align="left"
+    :ui="{
+      strategy: 'override',
+      description: 'text-lg mt-10',
+      title: 'text-4xl font-bold tracking-tight text-primary',
+    }"
   >
+    <template #description>
+      Chez INOA Solutions, nous sommes avant tout des passionnés de technologie
+      et d’innovation.
+      <br />
+      <br />
+      C’est la raison pour laquelle nous vous proposons ce qui se fait de mieux
+      sur le marché :
+      <br />
+      <br />
+      <div class="flex items-center gap-4 text-sm">
+        - Reconnaissance automatique de plaque d’immatriculation avec Dahua
+        Technology
+        <br />
+        <br />
+        - Caméra nomade La Chouette pour sécuriser une commune intelligemment,
+        <br />
+        <br />
+        - Détection intrusion par caméra thermique…
+      </div>
+      <br />
+      <br />
+      Nous avons forcément la solution adaptée à vos besoins.
+    </template>
     <img
       src="https://picsum.photos/640/360"
       class="w-full rounded-md shadow-xl ring-1 ring-gray-300 dark:ring-gray-700"
